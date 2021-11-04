042ea01ab4ef6b74038ce40d4b7366066ac55b12
tamas1023 <bodnar.tamas.1023@gmail.com>
2021-11-04 11:31:47 +0100

    Képek beszúrása és tábla generálása

--- Damajatek/Form1.Designer.cs
+++ Damajatek/Form1.Designer.cs
@@ -35,45 +35,63 @@
             this.startBTN = new System.Windows.Forms.Button();
             this.keszitokBTN = new System.Windows.Forms.Button();
             this.leirasBTN = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // nev1TBX
             // 
+            this.nev1TBX.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.nev1TBX.Location = new System.Drawing.Point(659, 159);
             this.nev1TBX.Name = "nev1TBX";
-            this.nev1TBX.Size = new System.Drawing.Size(100, 20);
+            this.nev1TBX.Size = new System.Drawing.Size(100, 29);
             this.nev1TBX.TabIndex = 0;
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox1.Location = new System.Drawing.Point(659, 94);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(584, 97);
+            this.label1.BackColor = System.Drawing.Color.Turquoise;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(565, 97);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(88, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "Első név:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(571, 159);
+            this.label2.BackColor = System.Drawing.Color.Turquoise;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(532, 164);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.Size = new System.Drawing.Size(121, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Második név:";
             // 
             // startBTN
             // 
-            this.startBTN.Location = new System.Drawing.Point(684, 38);
+            this.startBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startBTN.Location = new System.Drawing.Point(659, 38);
             this.startBTN.Name = "startBTN";
-            this.startBTN.Size = new System.Drawing.Size(75, 23);
+            this.startBTN.Size = new System.Drawing.Size(100, 40);
             this.startBTN.TabIndex = 4;
             this.startBTN.Text = "Start";
             this.startBTN.UseVisualStyleBackColor = true;
@@ -81,26 +99,89 @@
             // 
             // keszitokBTN
             // 
-            this.keszitokBTN.Location = new System.Drawing.Point(684, 227);
+            this.keszitokBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.keszitokBTN.Location = new System.Drawing.Point(659, 227);
             this.keszitokBTN.Name = "keszitokBTN";
-            this.keszitokBTN.Size = new System.Drawing.Size(75, 23);
+            this.keszitokBTN.Size = new System.Drawing.Size(100, 40);
             this.keszitokBTN.TabIndex = 5;
             this.keszitokBTN.Text = "Készítők";
             this.keszitokBTN.UseVisualStyleBackColor = true;
             // 
             // leirasBTN
             // 
-            this.leirasBTN.Location = new System.Drawing.Point(684, 290);
+            this.leirasBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.leirasBTN.Location = new System.Drawing.Point(659, 290);
             this.leirasBTN.Name = "leirasBTN";
-            this.leirasBTN.Size = new System.Drawing.Size(75, 23);
+            this.leirasBTN.Size = new System.Drawing.Size(100, 40);
             this.leirasBTN.TabIndex = 5;
             this.leirasBTN.Text = "Leírás";
             this.leirasBTN.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Turquoise;
+            this.panel1.Location = new System.Drawing.Point(528, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 426);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Turquoise;
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(20, 20);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Damajatek.Properties.Resources.feher;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Damajatek.Properties.Resources.feherd;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Damajatek.Properties.Resources.fekete;
+            this.pictureBox3.Location = new System.Drawing.Point(100, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Damajatek.Properties.Resources.feketed;
+            this.pictureBox4.Location = new System.Drawing.Point(150, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.leirasBTN);
             this.Controls.Add(this.keszitokBTN);
@@ -109,8 +190,16 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.nev1TBX);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "Form1";
+            this.Text = "Dámajáték";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +214,12 @@
         private System.Windows.Forms.Button startBTN;
         private System.Windows.Forms.Button keszitokBTN;
         private System.Windows.Forms.Button leirasBTN;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Damajatek/Form1.cs
+++ Damajatek/Form1.cs
@@ -18,12 +18,68 @@ namespace Damajatek
         {
             InitializeComponent();
             //damak feltöltése fele fehér fele fekete
+            damafeltoltes();
+            tablageneralas();
+        }
+
+        private void tablageneralas()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                //i megy soronként
+                for (int j = 0; j < 8; j++)
+                {
+                    // j megyoszloponként
+                    PictureBox kep = new PictureBox();
+                    kep.Location = new System.Drawing.Point(20+(i*50),20+(j*50));
+                    kep.Name =j+"";
+                    kep.Visible = true;
+                    kep.Size = new System.Drawing.Size(50, 50);
+                    if (i%2==0)
+                    {
+                        if (j%2==0)
+                        {
+                            kep.BackColor = Color.White;
+                        }
+                        else
+                        {
+                            kep.BackColor = Color.Black;
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                        {
+                            kep.BackColor = Color.Black;
+                        }
+                        else
+                        {
+                            kep.BackColor = Color.White;
+                        }
+                    }
+                    kep.Tag = i + "";
+                    kep.Image = Image.FromFile("feher.png");
+                    kep.SizeMode = PictureBoxSizeMode.StretchImage;
+                    Controls.Add(kep);
+                    //kep.BringToFront();
+
+                    //kep.Click += new System.EventHandler(this.palyaklikk);
+                }
+            }
+            panel2.SendToBack();
+            pictureBox4.BringToFront();
+        }
+
+        private void damafeltoltes()
+        {
             for (int i = 0; i < damak.Length; i++)
             {
-                if(i>damak.Length/2)
-                { 
-                damak[i].Szin = "feher";
-                damak[i].Damae = false;
+                damak[i] = new Dama("", false);
+                if (i > damak.Length / 2)
+                {
+                    damak[i].Szin = "feher";
+                    damak[i].Damae = false;
                 }
                 else
                 {
@@ -31,8 +87,6 @@ namespace Damajatek
                     damak[i].Damae = false;
                 }
             }
-
-        
         }
 
         private void Form1_Load(object sender, EventArgs e)
