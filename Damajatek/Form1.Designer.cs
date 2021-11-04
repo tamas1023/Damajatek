adc40d7492f42ac499ed6f736f7b9ca69b5e025e
tamas1023 <bodnar.tamas.1023@gmail.com>
2021-11-04 09:35:06 +0100

    teszt commit

--- Damajatek/Form1.Designer.cs
+++ Damajatek/Form1.Designer.cs
@@ -28,22 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nev1TBX = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // nev1TBX
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.nev1TBX.Location = new System.Drawing.Point(60, 51);
+            this.nev1TBX.Name = "nev1TBX";
+            this.nev1TBX.Size = new System.Drawing.Size(100, 20);
+            this.nev1TBX.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.nev1TBX);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,7 +53,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox nev1TBX;
     }
 }
 
